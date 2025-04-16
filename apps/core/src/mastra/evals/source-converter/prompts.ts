1d8174794de9f7a97129c6e327b787be4eabb16f
Charles Martinot <charles.martinot@protonmail.com>
2025-04-16 11:59:20 +0800

    Update judge prompt

--- apps/core/src/mastra/evals/source-converter/prompts.ts
+++ apps/core/src/mastra/evals/source-converter/prompts.ts
@@ -1,9 +1,14 @@
 export const SOURCE_CODE_INSTRUCTIONS = `You are a senior software engineer in charge of reviewing source code converted from the awscdk project into our own projects called terraconstructs.`
 
-// TODO: flesh this out
-export const generateSourceCodePrompt = ({ sourceCode }: { sourceCode: string }) => `Please review the following source code and check if it uses any constructs from the terraconstructs project. 
+export const generateSourceCodePrompt = ({ sourceCode }: { sourceCode: string }) => `Please review the following source code according to the following rules. 
 
-Check for awscdk constructs that have been replaced with terraconstructs constructs.
+Check that the converted code uses all the TerraConstruct AWS Specific constructs for the conversion and pay extra attention that the following are true: 
+- it extends the abstract AwsConstructBase class where AWSCDK extends the Resource class 
+- the Constructor Props also extend the AwsConstructProps Interface 
+- it provides an implementation for the abstract outputs getter exposing primitive AWSCDK Construct Interface attributes 
+- it uses the AwsStack utility attributes to closely match AWSCDK Stack behaviour 
+If all of these are true then we can consider that it is properly using Terraconstructs and "usesConstructs" should be true.
+Else if any of these are not true then we can consider that it is not properly using Terraconstructs and "usesConstructs" should be false.
 
 Source code to analyse:
 \`\`\`
@@ -13,13 +18,13 @@ ${sourceCode}
 Return your response in this format:
 {
   "usesConstructs": boolean
+  "falseRules": ["list of rules that are false when checking the converted code."]
 }
 `;
 
-// TODO flesh this out
 export const generateReasonPrompt = ({ usesConstructs }: { usesConstructs: boolean }) => `Explain why this converted source code ${usesConstructs ? 'uses' : 'does not use'} constructs from the terraconstructs project.
 
-Return your response in this format: 
+Return your response in this format, giving the list of checks that are false: 
 {
-  "reason": "This file is [valid/invalid] because it [uses/does not use] constructs from the terraconstructs project."
+  "reason": "This file is [valid/invalid] because [explanation]."
 }`;
